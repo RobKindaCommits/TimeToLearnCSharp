c737aaaf9fb2952f9dffbf0875f651bcb2f11e83
RobKindaCommits <robert.p.fenoglio@gmail.com>
2025-12-29 08:39:18 -0500

    Adding the counting vowels exercise

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+/*
 Console.WriteLine("Hello, World!");
 string myVariable = "This fruit is an apple";
-Type dataType = myVariable.GetType();
+string myVariableTwo = "123";
+*/
+
+/*Type dataType = myVariable.GetType();
 Console.WriteLine(dataType);
 if (dataType == typeof(int))
 {
@@ -10,4 +15,31 @@ if (dataType == typeof(int))
 else
 {
     Console.WriteLine ($"The variable {myVariable} is not an integer.");
+}*/
+
+
+/*
+if (int.TryParse(myVariableTwo, out int number))
+{
+    Console.WriteLine($"Conversion successful: {number}");
 }
+else
+{
+    Console.WriteLine("Conversion failed");
+}
+*/
+
+//Counting vowels
+Console.WriteLine("Enter a string:");
+string inputString = Console.ReadLine();
+//Console.WriteLine ("You entered " + inputString);
+int vowelCount = 0;
+foreach (char c in inputString)
+{
+    if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
+    {
+        vowelCount++;
+    }
+}
+string message = $"The vowel count in {inputString} is {vowelCount}.";
+Console.WriteLine(message);
--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c30232333fbd80f9951fc1143dcc93a0c2ce2b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7940989cf17917a087bf30c687a9c40672d72192")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
